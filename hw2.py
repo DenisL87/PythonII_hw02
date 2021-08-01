99a22ccc295b75ac21598a7e306724dfe089f374
DenisL87 <denis.litvinenko@yahoo.com>
2021-08-01 21:53:23 +0300

    ...

--- hw2.py
+++ hw2.py
@@ -6,10 +6,20 @@ import csv
 app = Flask(__name__)
 fake = Faker()
 
-@app.route('/requirements')
+@app.route('/requirements/')
 def req_func ():
     with open('requirements.txt', 'r') as file:
         return file.read()
+
+@app.route('/space/')
+def astronauts_No():
+    r = requests.get('http://api.open-notify.org/astros.json')
+    dict = eval(r.text)
+    value = 0
+    for key in dict:
+        if key == 'number':
+            value = dict[key]
+    return str(value)
 
 @app.route('/mean/')
 def mean():
@@ -35,7 +45,7 @@ def mean():
             count += 1
         weightCount += 2
     meanweight = totalWeight / count
-    return f'mean height is {meanheight}, mean weight is {meanweight}'
+    return f'mean height is {meanheight}/n mean weight is {meanweight}'
 
 if __name__ == '__main__':
     app.run(debug=True)
