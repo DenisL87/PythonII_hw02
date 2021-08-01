a1174e4988ad429c5ad40674614724c9ee6311b3
DenisL87 <denis.litvinenko@yahoo.com>
2021-08-01 15:14:17 +0300

    ...

--- hw2.py
+++ hw2.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 @app.route('/requirements')
 def req_func ():
     with open('requirements.txt', 'r') as file:
-        text = file.read()
-        print(text)
-    return text
+        return file.read()
 
 if __name__ == '__main__':
     app.run(debug=True)
