b2a6f4c37afddb76835d1c841003e2360e4eeac7
DenisL87 <denis.litvinenko@yahoo.com>
2021-08-02 20:42:52 +0300

    ...

--- hw2.py
+++ hw2.py
@@ -18,9 +18,10 @@ def req_func ():
         print(text)
     return text
 
-@app.route('/generate-users/')
-def gen_users ():
+@app.route('/generate-users/<getusers>')
+def gen_users (getusers):
     users = {}
+    newdict = {}
     count = 0
     while count < 100:
         name = fake.name()
@@ -29,7 +30,11 @@ def gen_users ():
             email = email.replace(' ', '')
         users[count + 1] = ['name: ' + name + ', email: ' + email]
         count += 1
-    return users
+    count = 0
+    while count < int(getusers):
+        newdict[count + 1] = users[count + 1]
+        count += 1
+    return newdict
 
 @app.route('/mean/')
 def mean():
@@ -43,7 +48,7 @@ def mean():
         if text[heightCount] != '':
             totalHeight += float(text[heightCount])
             count += 1
-        heightCount += 2
+        heightCount += 3
     meanheight = (totalHeight / count) * 2.54
 
     weightCount = 5
@@ -53,7 +58,7 @@ def mean():
         if text[weightCount] != '':
             totalWeight += float(text[weightCount])
             count += 1
-        weightCount += 2
+        weightCount += 3
     meanweight = (totalWeight / count) / 2.20462
     return f'mean height is {meanheight} cm, mean weight is {meanweight} kg'
 
